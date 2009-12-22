14142ab24e569e0c34d407e64b5eb003a14a2b1d
Simon Kallweit <simon.kallweit@intefo.ch>
2009-12-22 23:37:40 +0100

    removed leftovers

--- src/camcontrol/param_def.h
+++ src/camcontrol/param_def.h
@@ -1,22 +1,4 @@
 
-#if 0
-struct param_data {
-	uint16_t focal_length;		/**< Focal length (mm * 0.1) */
-	uint16_t sensor_width;		/**< Sensor width (mm * 0.1) */
-	uint16_t sensor_height;		/**< Sensor height (mm * 0.1) */
-	uint16_t fov_h;				/**< Horizontal field of view (deg * 0.01) */
-	uint16_t fov_v;				/**< Vertical field of view (deg * 0.01) */
-	uint16_t pan_width;			/**< Panorama width (deg) */
-	uint16_t pan_height;		/**< Panorama height (deg) */
-	uint8_t shutter_mode;		/**< Shutter mode */
-	uint8_t hdr_time1;			/**< Min exposure time for HDR series */
-	uint8_t hdr_time2;			/**< Max exposure time for HDR series */
-	uint16_t hdr_shots;			/**< Number of shots per HDR series */
-	uint16_t trigger_delay;		/**< Trigger delay between shots in s * 0.01 */
-	uint32_t timelapse_rate;	/**< Timelapse trigger rate in s */
-};
-#endif
-
 PARAM_CLASS(pc_focal_length, UINT16, 100, 500, 200, 1, 10, print_mm)
 PARAM_CLASS(pc_sensor_dim, UINT16, 100, 500, 200, 1, 10, print_mm)
 //PARAM_CLASS(pc_deg_180, UINT16, 0, 180, 90, 1, 10, print_mm)
