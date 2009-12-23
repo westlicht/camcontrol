41bd9423632fc8f3caf07a65c29d87401ae98ff3
Simon Kallweit <simon.kallweit@intefo.ch>
2009-12-23 23:46:48 +0100

    added debugging on serial port

--- src/camcontrol/debug.h
+++ src/camcontrol/debug.h
@@ -9,13 +9,26 @@
 #define __DEBUG_H__
 
 #include <avr/io.h>
+#include <stdio.h>
 #include "lcd.h"
 
+/** Set for debugging output */
+#define DEBUG
+
+/* Debug LED */
 #define DBG_LED_ON()	PORTA |= _BV(6)
 #define DBG_LED_OFF()	PORTA &= ~_BV(6)
 
+/* Debug print */
+#ifdef DEBUG
+# define DBG(_fmt_, _args_...) printf(_fmt_, ##_args_)
+#else
+# define DBG(_fmt_, _args_...)
+#endif
+
 void on_assert(const char *filename, uint16_t line);
 
+/** Assertion macro */
 #define ASSERT(_expr_)							\
 	do {										\
 		if (!(_expr_)) {						\
